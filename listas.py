15826361560f670baa0d593bec6b2fa687230a0c
Flow <magebrnet@gmail.com>
2024-11-07 20:18:52 -0400

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,11 @@
+lista = []
+
+lista1 = []
+
+lista = [0]*6
+
+#list comprehension
+
+lista3 = ['_' for x in range(10)]
+
+print (lista3)
---
+++ matrizes.py
@@ -0,0 +1,6 @@
+lista_matriz = [[1,2],[3,4]]
+
+for n in range(2):
+    for m in range(2):
+        print(lista_matriz[n][m])
+        
