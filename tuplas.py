09295d7032b44b80904e58339f2f600f62ea09ce
Flow <magebrnet@gmail.com>
2024-11-08 11:00:07 -0400

    teste envio

--- tuplas.py
+++ tuplas.py
@@ -7,7 +7,7 @@ dias = ('domingo' , 'segunda', 'terca')
 # DICIONARIOS
 
 pessoa = {
-    'name': 'nicolass',
+    'name': 'nicollllaasssss',
     'age': '21',
     'adress': 'rua x',
     'number': '123'
